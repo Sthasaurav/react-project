10f87b2b07ef5af36b19f4b3b0ccb6f435200b9f
Sthasaurav <92661343+Sthasaurav@users.noreply.github.com>
2024-05-27 10:59:14 +0545

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import Accoridian from './components/accoridian/accoridian';
+import UseState from './components/hooks/UseState';
 
  function App() {
   return <div>
-      <Accoridian/>
+      {/* <Accoridian/> */}
+      <UseState/>
     </div>
   
 }
